b359043bcb257f70ef2f7b902b18a0f9a5c7b5b0
Anim Mostafa <bm.anim420@gmail.com>
2018-05-30 04:17:32 +0000

    Task2a

--- script.js
+++ script.js
@@ -6,7 +6,16 @@ console.log(heading.innerText);
 
 
 /* Task 2 using plain JavaScript */
-
+//TASK 2a
+var textBtn = document.getElementById('textBtn2');
+textBtn.addEventListener('click', function(){
+    var task2 = document.getElementById('task2a');
+    var paragraph = document.createElement('p');
+    paragraph.innerText = "Hello World";
+    //append the paragraph
+    task2.appendChild(paragraph);
+});
+//TASK 2b
 
 
 /* Task 4 using jQuery */
